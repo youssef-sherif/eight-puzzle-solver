c4e22751459dfa5229aca5e7f5307c8c443d9cdd
Youssef Sherif <youssefsabry97@gmail.com>
2019-07-31 16:32:39 +0200

    Fix issue with board where multiple movements were not working properly

--- Board.py
+++ Board.py
@@ -2,31 +2,50 @@
 # Board class
 #############
 
+
 class Board:
     tiles = {}
+    actions = list()
+
+    def __eq__(self, board: 'Board') -> 'bool':
+        for key in list(self.tiles.keys()):
+            if key not in list(board.tiles.keys()):
+                return False
+        return True
 
     def __init__(self):
         return
 
     @classmethod
-    def from_dictionary(cls, tiles: {}) -> 'Board':
+    def from_previous(cls, tiles: {}) -> 'Board':
         cls.tiles = tiles
-        cls.empty_tile_location = get_empty_tile_location(cls.tiles)
+        cls.empty_tile_location = int(get_empty_tile_location(cls.tiles))
 
         return cls()
 
     @classmethod
-    def from_input(cls, size: int) -> 'Board':
-        for i in range(0, size):
-            cls.tiles[i] = int(input("Enter tile " + str(i) + " value: "))
-            print(cls.tiles)
-        cls.empty_tile_location = get_empty_tile_location(cls.tiles)
+    def from_array(cls, numbers: []) -> 'Board':
+        tiles = {}
+        cls.actions = list()
+        for i in range(0, 9):
+            tiles[i] = numbers[i]
+        cls.tiles = tiles
+        cls.empty_tile_location = int(get_empty_tile_location(cls.tiles))
 
         return cls()
 
+    def solution_json(self):
+
+        return {
+            'actions': self.actions,
+            'solution': self.tiles
+        }
+
     def up(self) -> 'Board':
         if self.can_move_up():
-            board = swap(self.empty_tile_location - 3, self.empty_tile_location, self)
+            board = Board.from_previous(self.tiles)
+            board = swap(self.empty_tile_location - 3, self.empty_tile_location, board)
+            self.actions.insert(self.actions.__len__(), 'up')
 
             return board
         else:
@@ -34,7 +53,9 @@ class Board:
 
     def down(self) -> 'Board':
         if self.can_move_down():
-            board = swap(self.empty_tile_location + 3, self.empty_tile_location, self)
+            board = Board.from_previous(self.tiles)
+            board = swap(self.empty_tile_location + 3, self.empty_tile_location, board)
+            self.actions.insert(self.actions.__len__(), 'down')
 
             return board
         else:
@@ -42,7 +63,9 @@ class Board:
 
     def left(self) -> 'Board':
         if self.can_move_left():
-            board = swap(self.empty_tile_location - 1, self.empty_tile_location, self)
+            board = Board.from_previous(self.tiles)
+            board = swap(self.empty_tile_location - 1, self.empty_tile_location, board)
+            self.actions.insert(self.actions.__len__(), 'left')
 
             return board
         else:
@@ -50,7 +73,9 @@ class Board:
 
     def right(self) -> 'Board':
         if self.can_move_right():
-            board = swap(self.empty_tile_location + 1, self.empty_tile_location, self)
+            board = Board.from_previous(self.tiles)
+            board = swap(self.empty_tile_location + 1, self.empty_tile_location, board)
+            self.actions.insert(self.actions.__len__(), 'right')
 
             return board
         else:
@@ -77,16 +102,17 @@ class Board:
 # helper methods
 ################
 
+
 def get_empty_tile_location(tiles: {}) -> 'int':
     for tile in tiles.keys():
         if tiles[tile] == 0:
             return tile
 
 
-def swap(x: int, y: int, old_board: Board) -> 'Board':
-    board = Board.from_dictionary(old_board.tiles)
+def swap(x: int, y: int, board: 'Board') -> 'Board':
     temp = board.tiles[x]
     board.tiles[x] = board.tiles[y]
     board.tiles[y] = temp
 
     return board
+
