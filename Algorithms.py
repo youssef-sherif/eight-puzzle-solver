a391a8a954beb0ffd20a5b1797ea4fc208050336
Youssef Sherif <youssefsabry97@gmail.com>
2019-08-01 00:41:21 +0200

    BFS probably working (takes alot of time for random arrays so I don't know)

--- Algorithms.py
+++ Algorithms.py
@@ -1,13 +1,15 @@
 import queue
 from Board import Board
+from Node import Node
+import random
 import heapq
 
 
 class Algorithms:
 
-    def __init__(self):
-        board = Board.from_array(random.sample(range(0, 9), 9))
-        self.initialState = Node(board)
+    def __init__(self, array: []):
+        board = Board.from_array(array)
+        self.initial_state = Node(board)
         self.goal = Board.from_array([0,1, 2, 3, 4, 5, 6, 7, 8])
         return
 
@@ -16,22 +18,43 @@ class Algorithms:
         frontier = queue.Queue()
         expanded = 0
         frontier.put_nowait(self.initial_state)
-        explored = set()
+        explored = []
 
         while not frontier.empty():
             state = frontier.get_nowait()
-            explored.add(state)
+
+            if state.board.tiles in explored:
+                continue
+
+            if state.board.__eq__(self.goal):
+                return True
+
+            explored.append(state.board.tiles)
+            expanded += 1
+
+            state.set_children()
+
+            for child in state.children:
+                print(child.board.tiles)
+                frontier.put_nowait(child)
+
+        return False
+
+    def a_star_search(self, initial_state: Board, goal: Board) -> 'bool':
+
+        frontier = queue.PriorityQueue()
+        explored = set()
+
+        while not p_queue.empty():
+            state = frontier.get_nowait()
+
+            if state.board.tiles in explored:
+                continue
 
             if state.board.__eq__(self.goal):
 
                 return True
-
-            for neighbor in state.get_neighbours():
-                if neighbor not in frontier and neighbor not in explored:
-                    frontier.put_nowait(neighbor)
-
-        return False
-
+        return
 
     def dfs_search(self, initial_state: Board, goal: Board) -> 'bool':
 
@@ -53,13 +76,6 @@ class Algorithms:
 
         return False
 
-
-    def a_star_search(self, initial_state: Board, goal: Board) -> 'bool':
-
-        heapq.heapify([initial_state])
-        explored = set()
-
-        return
 
     def solution_json(self):
 
