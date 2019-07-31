d373b8c2be7661ae718e65fcacc2bbbfe5ba112a
Mohamed Tarek <mtarek.22796@gmail.com>
2019-07-31 18:17:21 +0200

    tree

--- Tree.py
+++ Tree.py
@@ -3,69 +3,103 @@ from Board import Board
 
 class Node:
     children = []
+
     def __init__(self, input_board: Board):
+
         self.board = input_board
         self.up = None
         self.down = None
         self.left = None
         self.rigth = None
-        self.parent = None
         return
+
+    @classmethod
+    def from_node(cls, input_board: Board, parent: 'Node'):
+        cls.parent = parent
+        return cls(input_board)
 
     def get_board(self):
         return self.board
 
     def set_up(self):
-        self.up = Node(self.board.up())
-        self.children.append(self.up)
-        return self.up
-
+        try:
+            self.up = Node.from_node(self.board.up(), self)
+            self.children.append(self.up)
+            return self.up
+        except Exception as e:
+            print(e)
     def set_down(self):
-        self.down = self.board.down()
-        self.children.append(self.down)
+        try:
+            self.down = Node.from_node(self.board.down(),self)
+            self.children.append(self.down)
+        except Exception as e:
+            print(e)
         return self.down
 
     def set_left(self):
-        self.left = self.board.left()
-        self.children.append(self.left)
+        try:
+            self.left = Node.from_node(self.board.left(),self)
+            self.children.append(self.left)
+        except Exception as e:
+            print(e)
         return self.left
 
     def set_right(self):
-        self.rigth = self.board.right()
-        self.children.append(self.rigth)
+        try:
+            self.rigth = Node.from_node(self.board.right(),self)
+            self.children.append(self.rigth)
+        except Exception as e:
+            print(e)
         return self.rigth
-class Tree:
 
-    list = []
-
-    # def build(self,root: Node):
-    #     self.list.append(root.set_up())
-    #     self.list.append(root.set_down())
-    #     self.list.append(root.set_left())
-    #     self.list.append(root.set_right())
-    #     if len(self.list)<30:
-    #         for child in root.children:
-    #             self.build(child)
-    #     return
-    #
-    # def build(self,root : Node):
-    #     queue = Queue()
-    #     queue.enqueue(self.root.set_up())
-    #     queue.enqueue(self.root.set_down())
-    #     queue.enqueue(self.root.set_left())
-    #     queue.enqueue(self.root.set_right())
-    #     while queue.length >0 and queue.length<30:
-    #
-    #     return
-    #
-    def __init__(self,board : Board):
-        self.root = Node.__init__(board)
-        self.build(self.root)
+    def set_children(self):
+        self.set_up()
+        self.set_down()
+        self.set_left()
+        self.set_right()
         return
 
+    def get_neighbours(self):
+        neighbours = [self.parent]
+        neighbours.extend(self.children)
+        return neighbours
 
-    def insert(self, node: Node):
-        self.list.append([node])
+    def build(self):
 
-    def get_child(self, node: Node):
         return
+#
+# class Tree:
+#
+#     list = []
+#
+#     # def build(self,root: Node):
+#     #     self.list.append(root.set_up())
+#     #     self.list.append(root.set_down())
+#     #     self.list.append(root.set_left())
+#     #     self.list.append(root.set_right())
+#     #     if len(self.list)<30:
+#     #         for child in root.children:
+#     #             self.build(child)
+#     #     return
+#     #
+#     # def build(self,root : Node):
+#     #     queue = Queue()
+#     #     queue.enqueue(self.root.set_up())
+#     #     queue.enqueue(self.root.set_down())
+#     #     queue.enqueue(self.root.set_left())
+#     #     queue.enqueue(self.root.set_right())
+#     #     while queue.length >0 and queue.length<30:
+#     #
+#     #     return
+#     #
+#     def __init__(self,board : Board):
+#         self.root = Node(board)
+#         self.build(self.root)
+#         return
+#
+#
+#     def insert(self, node: Node):
+#         self.list.append([node])
+#
+#     def get_child(self, node: Node):
+#         return
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from sklearn.metrics.pairwise import manhattan_distances
-from sklearn.neighbors import DistanceMetric
-from Board import Board
-
-board = Board.from_input(9)
-
-try:
-    board = board.up()
-    print(board.tiles)
-except Exception as e:
-    print(e)
-
+# from sklearn.metrics.pairwise import manhattan_distances
+# from sklearn.neighbors import DistanceMetric
+# from Board import Board
+#
+# board = Board.from_input(9)
+#
+# try:
+#     board = board.up()
+#     print(board.tiles)
+# except Exception as e:
+#     print(e)
+import random
 ###########################
 # this will be useful later
 ###########################
@@ -19,3 +19,17 @@ except Exception as e:
 #          [3, 4, 5]]
 #
 # array = dist.pairwise(X)
+
+from Tree import Node
+from Board import Board
+x= random.sample(range(0, 9), 9)
+try:
+    board= Board.from_array(x)
+except Exception as e:
+    print(e)
+node = Node(board)
+print(node.board.tiles)
+node.set_children()
+print(node.board.tiles)
+print(node.children)
+print(node.children[0].parent.board.tiles)
